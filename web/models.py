68b6b2ba5a981783d72e5368e0aa304f8b172283
shilpasasidharan97 <shilpasasidharan97@gmail.com>
2023-06-29 22:28:00 +0530

    slugify-helpers.py

--- web/models.py
+++ web/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 from froala_editor.fields import FroalaField
+from .helpers import *
 
-# Create your models here.
+
 class BlogModel(models.Model):
     title = models.CharField(max_length=1000)
     content = FroalaField()
@@ -11,3 +12,11 @@ class BlogModel(models.Model):
     image = models.ImageField(upload_to='blog')
     created_at = models.DateTimeField(auto_now_add=True)
     upload_to = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+    def save(self, *args, **kwargs):
+        self.slug = generate_slug(self.title)
+        super(BlogModel, self).save(*args, **kwargs)
