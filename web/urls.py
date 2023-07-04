90d39120e8247ebdfc218515665e07641feb648c
shilpasasidharan97 <shilpasasidharan97@gmail.com>
2023-07-04 22:18:10 +0530

    views and urls setting

--- web/urls.py
+++ web/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('login/', views.login_view, name="login_view"),
     path('logout-view/', views.logout_view, name="logout_view"),
     path('register/', views.register_view, name="register_view"),
+    path('add-blog/',views.add_blog, name='add_blog'),
+    path('blog-details/', views.blog_details, name='blog_details'),
+    path('category/', views.category, name='category')
 ]
--- web/views.py
+++ web/views.py
@@ -18,3 +18,15 @@ def logout_view(request):
 
 def register_view(request):
     return render(request, 'web/register.html')
+
+
+def add_blog(request):
+    return render(request, 'web/add_blog.html')
+
+
+def blog_details(request):
+    return render(request, 'web/blog_details.html')
+
+
+def category(request):
+    return render(request, 'web/category.html')
