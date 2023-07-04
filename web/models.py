b104e715a40186ff6f5702aa4a3f3efb32fdd2d4
shilpasasidharan97 <shilpasasidharan97@gmail.com>
2023-07-04 22:33:09 +0530

    model updated

--- web/models.py
+++ web/models.py
@@ -4,11 +4,17 @@ from froala_editor.fields import FroalaField
 from .helpers import *
 
 
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    is_verified = models.BooleanField(default=False)
+    token = models.CharField(max_length=100)
+
+
 class BlogModel(models.Model):
     title = models.CharField(max_length=1000)
     content = FroalaField()
     slug = models.SlugField(max_length=1000, null=True, blank=True)
-    # user = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='blog')
     created_at = models.DateTimeField(auto_now_add=True)
     upload_to = models.DateTimeField(auto_now=True)
